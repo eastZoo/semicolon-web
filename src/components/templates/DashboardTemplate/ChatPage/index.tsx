d13b9624273487a81413f5ecfab2752d8f21fb77
동주 <67012957+eastZoo@users.noreply.github.com>
2024-02-22 10:12:22 +0900

    style : tailwind css 환경 세팅

--- src/components/templates/DashboardTemplate/ChatPage/index.tsx
+++ src/components/templates/DashboardTemplate/ChatPage/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const [socket, setSocket] = useState(undefined);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+  }, []);
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default ChatPage;
