9e9e74dd6521c8aa28ba64720a58f62da5faa223
동주 <67012957+eastZoo@users.noreply.github.com>
2024-02-23 10:32:00 +0900

    style : og세팅

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,6 +8,12 @@ export default function Head() {
       <meta name="theme-color" content="#ffffff" />
       <meta name="description" content="Project name" />
       <meta name="veiwport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        property="og:title"
+        content="SEMICOLON - 우리들의 사이드프로젝트 커뮤니티"
+      />
+      <meta property="og:type" content="article" />
+      <meta property="og:image" content="/assets/image/semicolon-logo.png" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
